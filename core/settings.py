9437c540172eec2e6089b6c534d240f7a00bf26f
John Doe <eduardoroa22@gmail.com>
2023-01-29 18:54:09 -0500

    cofiguramos el core

--- core/settings.py
+++ core/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 import environ
 
-env=environ.Env()
+env = environ.Env()
 environ.Env.read_env()
 
 ENVIRONMENT = env
@@ -11,22 +11,36 @@ ENVIRONMENT = env
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+#nombre del sitio
 SITE_NAME = 'blog'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
-# Application definition
+ALLOWED_HOSTS = [
+        
+        "localhost",
+        "127.0.0.1",
+    ]
 
-INSTALLED_APPS = [
+if not DEBUG:
+     ALLOWED_HOSTS = [
+        "geler.click",
+        ".geler.click",
+        "www.geler.click", 
+    ]
+
+# esto lo pide render para desplegar el proyecto
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# separemos nuestras aplicaciones de manera mas amigable y que se entienda mejor
+DJANGO_APPS =[
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -35,8 +49,36 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+PROJECT_APPS =[
+    
+]
+
+THIRD_PARTY_APPS =[
+    'corsheaders',
+    'rest_framework',
+    'ckeditor',
+    'ckeditor_uploader',
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
+
+
+#configuramos ckeditor
+CKEDITOR_CONFIGS = {
+    'default' : {
+        'toolbar' : 'full',
+        'autoParagraph' : False
+    }
+} 
+CKEDITOR_UPLOAD_PATH = "/media"
+
+
+#modificamos el middleware
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #esto permite a acceder a las url de react
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # y este otro
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -47,10 +89,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
+#hacer coneccion entre Django rest framework y react
+# primero corremos "npm run build" y al hacer esto se va crear una carpeta llamada "build"
+# con esto podemos acceder a las platillas de react
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')], # con esto accedemos a la carpeta que creamos anteriormente 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,6 +122,38 @@ DATABASES = {
     }
 }
 
+DATABASES['default']['ATOMIC_REQUEST'] = True # con esto hacemos que la base de datos sea atomica y permita hacer request y que no se duplique la informacion
+
+# ponemos el Froend de react
+# cuando de despliega en el servidor debemos cambiar estas direcciones por el dominio y la ip del servidor
+CORS_ORIGIN_WHILIST =[
+    'http://localhost:3000',
+    'http://localhost:8000'
+]
+
+CSRF_TRUSTED_ORIGINS =[
+    'http://localhost:3000',
+    'http://localhost:8000'
+]
+
+if not DEBUG:
+    CORS_ORIGIN_WHILIST =[
+    'https://geler22.click',
+    'https://admin.geler22.click',
+    ]
+
+    CSRF_TRUSTED_ORIGINS =[
+        'https://geler22.click',
+        'https://admin.geler22.click',
+    ]
+    
+# agregamos estos "hashers" para proteccion extra para la base de datos
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BcryptSHA256PasswordHasher",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -99,7 +177,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
@@ -108,12 +186,77 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+# configuramos nuestros archivos estaticos, debemos crear una carpeta llamada "media" para las imagenes
+# debemos ejectuar "python manage.py collectstatic", debemos esta en DEBUG = true
+# esto crear mi carpeta static en mi proyecto
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# lo mismo para mi carpeta media
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/media/'
+
+# luego nuestros archivos estaticos de react
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#configuramos Django rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE' : 16,
+}
+
+# luego hacemos la autenticacion de bakends
+AUTHENTICATION_BACKENDS = {
+    'django.contrib.auth.backends.ModelBackend',
+}
+
+FILE_UPLOAD_PERMISSIONS = 0o640
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+if not DEBUG:
+    DEFAULT_FROM_EMAIL = "uridium <eduardoroa22@gmail.com>"
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = env('EMAIL_HOST')
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = env('EMAIL_PORT')
+    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+    
+    #aqui esta toda la logica para usar los bucket de S3 de AWS
+    
+    # django-ckeditor will not work with S3 through django-storages without this line in settings.py
+    AWS_QUERYSTRING_AUTH = False
+
+    # aws settings
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+
+
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_DEFAULT_ACL = 'public-read'
+
+    # s3 static settings
+
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    # s3 public media settings
+
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStore'
